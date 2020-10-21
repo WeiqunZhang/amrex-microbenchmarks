738127fa3a84afe23c73962c6bb82c0420e91303
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-10-21 20:02:11 +0000

    reduce

---
+++ reduce/main.cpp
@@ -0,0 +1,46 @@
+#include <AMReX.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_Random.H>
+
+using namespace amrex;
+
+int main(int argc, char* argv[])
+{
+    amrex::Initialize(argc,argv);
+    {
+        BL_PROFILE("main()");
+
+        int max_grid_size = 256;
+        {
+            ParmParse pp;
+            pp.query("max_grid_size", max_grid_size);
+        }
+
+        Box domain(IntVect(0),IntVect(255));
+        BoxArray ba(domain);
+        ba.maxSize(max_grid_size);
+        DistributionMapping dm{ba};
+        MultiFab mf(ba,dm,1,0);
+        for (MFIter mfi(mf); mfi.isValid(); ++mfi) {
+            const Box& bx = mfi.validbox();
+            Array4<Real> const& a = mf.array(mfi);
+            amrex::ParallelForRNG(bx,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k, RandomEngine const& engine)
+            {
+                a(i,j,k) = amrex::Random(engine) + 0.5_rt;
+            });
+        }
+#ifdef AMREX_USE_DPCPP
+        {
+            BL_PROFILE("reduce-warmup");
+            mf.sum();
+        }
+#endif
+        {
+            BL_PROFILE("reduce-mf");
+            mf.sum();
+        }
+    }
+    amrex::Finalize();
+}
