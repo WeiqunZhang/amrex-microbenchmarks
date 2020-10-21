b6f4d0e5b87d87fd909910e65e4cf98fc938bd78
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-10-21 20:32:59 +0000

    scan

---
+++ scan/main.cpp
@@ -0,0 +1,47 @@
+#include <AMReX.H>
+#include <AMReX_iMultiFab.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_Scan.H>
+
+using namespace amrex;
+
+static void test (iMultiFab& mfa, iMultiFab& mfb)
+{
+    for (MFIter mfi(mfa); mfi.isValid(); ++mfi) {
+        Scan::ExclusiveSum(mfi.fabbox().numPts(), mfb[mfi].dataPtr(), mfa[mfi].dataPtr());
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    amrex::Initialize(argc,argv);
+    {
+        BL_PROFILE("main()");
+
+        int max_grid_size = 256;
+        {
+            ParmParse pp;
+            pp.query("max_grid_size", max_grid_size);
+        }
+
+        Box domain(IntVect(0),IntVect(255));
+        BoxArray ba(domain);
+        ba.maxSize(max_grid_size);
+        DistributionMapping dm{ba};
+        iMultiFab mfa(ba,dm,1,0);
+        iMultiFab mfb(ba,dm,1,0);
+        mfa.setVal(1);
+        mfb.setVal(1);
+#ifdef AMREX_USE_DPCPP
+        {
+            BL_PROFILE("scan-warmup");
+            test(mfa,mfb);
+        }
+#endif
+        {
+            BL_PROFILE("scan-mf");
+            test(mfa,mfb);
+        }
+    }
+    amrex::Finalize();
+}
