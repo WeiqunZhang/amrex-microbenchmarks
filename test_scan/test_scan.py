2db8f33656dd0e089e8dfe3ce07cfec3c677a3d6
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-11-12 19:32:16 -0800

    add report interval

--- test_scan/test_scan.py
+++ test_scan/test_scan.py
@@ -9,14 +9,17 @@ if len(sys.argv) < 2:
 
 parser = argparse.ArgumentParser(description="Scan test")
 parser.add_argument("--time", type=float)
+parser.add_argument("--report", type=float, default=1)
 parser.add_argument("--command", type=str, default="./main3d.gnu.TPROF.CUDA.ex")
 args = parser.parse_args(sys.argv[1:])
 
-tmax = time.perf_counter() + args.time
+tstart = time.perf_counter()
+tmax = tstart + args.time*60
 
 ntests_total = 0
 ntests_pass = 0
 
+t0 = time.perf_counter()
 while time.perf_counter() < tmax:
     p0 = subprocess.Popen(args.command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     stdout0, stderr0 = p0.communicate()
@@ -26,5 +29,9 @@ while time.perf_counter() < tmax:
     for line in stdout0.decode().split('\n'):
         if line.startswith("pass"):
             ntests_pass += 1
+    t1 = time.perf_counter()
+    if (t1-t0 > args.report*60):
+        t0 = t1
+        print("After", (t1-tstart)/60., "minutes", ntests_pass, "out of", ntests_total, "tests passed")
 
-print(ntests_pass, "out of", ntests_total, "tests passed")
+print("Final:", ntests_pass, "out of", ntests_total, "tests passed")
